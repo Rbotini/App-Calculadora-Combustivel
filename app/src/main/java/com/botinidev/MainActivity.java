317273e66b4731660f898982b8b432b52e58d26d
Renato Botini <botinirenato@gmail.com>
2022-01-31 17:49:12 -0300

    Função do Botão Calcular Criada

--- app/src/main/java/com/botinidev/MainActivity.java
+++ app/src/main/java/com/botinidev/MainActivity.java
@@ -3,12 +3,29 @@ package com.botinidev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = "Botao";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button button = findViewById(R.id.btn_calcular);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick: Clicou ");
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/botinidev/Splash.java
+++ app/src/main/java/com/botinidev/Splash.java
@@ -2,13 +2,39 @@ package com.botinidev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class Splash extends AppCompatActivity {
+
+    int tempo = 1000 * 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+             trocartela();
+            }
+        },tempo);
+
+
+
+
+
     }
+
+    private void trocartela() {
+       Intent intent = new Intent(Splash.this,MainActivity.class);
+       startActivity(intent);
+       finish();
+
+    }
+
 }

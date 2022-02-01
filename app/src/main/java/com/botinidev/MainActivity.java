feffae51fc34144058f00743c0efd3effede9fd5
Renato Botini <botinirenato@gmail.com>
2022-02-01 15:59:01 -0300

    Finalizado o metodo de calcular

--- app/src/main/java/com/botinidev/MainActivity.java
+++ app/src/main/java/com/botinidev/MainActivity.java
@@ -6,8 +6,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText alcoo,gasolina;
+    private TextView resultado;
 
     String TAG = "Botao";
 
@@ -16,13 +21,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         Button button = findViewById(R.id.btn_calcular);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG, "onClick: Clicou ");
+
+                alcoo = findViewById(R.id.editTextAlcoo);
+                gasolina = findViewById(R.id.editTextGasolina);
+                resultado = findViewById(R.id.resultado);
+                String  resulAlcoo = "Vale mais a pena usar Álcool!";
+                String  resulGasolina = "Vale mais a pena usar Gasolina!";
+
+                String editAlcoo = alcoo.getText().toString();
+                String editGasolina = gasolina.getText().toString();
+                double resulFinal = (Double.parseDouble(editAlcoo) / Double.parseDouble(editGasolina));
+                Log.i(TAG, "onClick: Clicou "+editAlcoo);
+                Log.i(TAG, "onClick: Clicou "+editGasolina);
+                Log.i(TAG, "onClick: Clicou "+resulFinal);
+
+                if (resulFinal > 0.7){
+                    resultado.setText(resulGasolina);
+                }else{
+                    resultado.setText(resulAlcoo);
+                }
+
             }
         });
     }
